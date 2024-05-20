5d47f55e6f2a3cffe77757fb2b60f78351ea6520
tjustsoup <tjustsoup@gmail.com>
2024-05-20 17:05:55 +0000

    ItemDrawer.tsx "onSave" mutation call - Added "@keystone-ui/toast" to imports (used for UI notifications) ln 80  - From "useCreateItem.ts", copied over the data serialization process ln 90  - Built the useMutation gql call ln 123 - Action passed to the confirm ("Save") button validates data, calls update, and handles successful update
    ⚠️ Still need to handle passing null or falsey values
    🛑 The "create" drawer is currently throwing errors when `onCreate` fires - will be fixing this ASAP

--- examples/agency/src/fields/virtual/views/processing_steps/components/ItemDrawer.tsx
+++ examples/agency/src/fields/virtual/views/processing_steps/components/ItemDrawer.tsx
@@ -4,13 +4,16 @@
 import { jsx, Box } from '@keystone-ui/core'
 import { Drawer } from '@keystone-ui/modals'
 import { LoadingDots } from '@keystone-ui/loading'
+import { useToasts } from '@keystone-ui/toast'
 
 import { useKeystone, useList } from '@keystone-6/core/admin-ui/context'
 
-import { Fields, deserializeValue, makeDataGetter } from '@keystone-6/core/admin-ui/utils'
+import { Fields, Value, deserializeValue, makeDataGetter } from '@keystone-6/core/admin-ui/utils'
 import { GraphQLErrorNotice } from '@keystone-6/core/admin-ui/components'
-import { gql, useQuery } from '@keystone-6/core/admin-ui/apollo'
-import { useEffect, useMemo, useState } from 'react'
+import { gql, useMutation, useQuery } from '@keystone-6/core/admin-ui/apollo'
+
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import isDeepEqual from 'fast-deep-equal'
 
 type ValueWithoutServerSideErrors = { [key: string]: { kind: 'value', value: any } }
 
@@ -72,6 +75,40 @@ export function ItemDrawer({
   }, [list, value])
   const [forceValidation, setForceValidation] = useState(false)
 
+  // "onSave" mutation call
+  const toasts = useToasts()
+  const updateData: Record<string, any> = {}
+  Object.keys(list.fields).forEach(fieldPath => {
+    const { controller } = list.fields[fieldPath]
+    const serialized = controller.serialize(value[fieldPath].value)
+    if (!isDeepEqual(serialized, controller.serialize(controller.defaultValue))) {
+      Object.assign(updateData, serialized)
+    }
+  })
+  const [updateItem, { loading, error, data: returnedData }] = useMutation(
+    gql`
+      mutation(
+        $where: ${list.gqlNames.whereUniqueInputName}!,
+        $data: ${list.gqlNames.updateInputName}!
+        ) {
+          ${list.gqlNames.updateMutationName} (
+            where: $where,
+            data: $data
+          ) {
+            ${Object.keys(list.fields)}
+          }
+        }
+    `,
+    {
+      variables: {
+        where: {
+          id: id
+        },
+        data: updateData
+      }
+    }
+  )
+
   return (
     <Drawer
       title={`Edit ${list.singular} > ${id}`}
@@ -81,7 +118,27 @@ export function ItemDrawer({
           label: `Save`,
           loading: itemState.loading,
           action: async () => {
+            // Validation check
+            const newForceValidation = invalidFields.size !== 0
+            setForceValidation(newForceValidation)
+            if (newForceValidation) return undefined
+
+            // Call update
+            let outputData;
+            try {
+              outputData = await updateItem()
+            } catch {
+              return undefined
+            }
+
+            // Handle success
+            toasts.addToast({
+              title: id,
+              message: "Updated Successfully",
+              tone: "positive"
+            })
             onSave(id)
+            return outputData
           },
         },
         cancel: {
@@ -117,7 +174,11 @@ export function ItemDrawer({
           forceValidation={forceValidation}
           invalidFields={invalidFields}
           value={value}
-          onChange={console.log}
+          onChange={useCallback(
+            (getNewValue: (value: Value) => Value) => {
+              setValue(oldValues => getNewValue(oldValues) as ValueWithoutServerSideErrors)
+            }, []
+          )}
         />
       </Box>
     </Drawer>
