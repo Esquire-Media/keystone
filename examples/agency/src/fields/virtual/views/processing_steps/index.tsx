8592d67eedab98dacec98cec6f37e4023b2b9fd8
tjustsoup <tjustsoup@gmail.com>
2024-05-21 15:38:02 +0000

    processing_steps/index.ts onCreate handling ln 61  - Added a query to get the entry that was just created. ln 75  - Added an onCreate handler to trigger query and handle the value onChange for the field. ✅ Creating objects now works properly. I believe MVP is achieved. ⚠️ Need to reformat and clean up code.

--- examples/agency/src/fields/virtual/views/processing_steps/index.tsx
+++ examples/agency/src/fields/virtual/views/processing_steps/index.tsx
@@ -3,7 +3,7 @@
 
 import { Button } from '@keystone-ui/button'
 import { Stack, jsx } from '@keystone-ui/core';
-import { DrawerController, Drawer } from '@keystone-ui/modals'
+import { DrawerController } from '@keystone-ui/modals'
 import { FieldProps } from '@keystone-6/core/types';
 import { FieldContainer, FieldDescription, FieldLabel } from '@keystone-ui/fields';
 import { controller } from '@keystone-6/core/fields/types/virtual/views';
@@ -12,6 +12,7 @@ import { Fragment, useState } from 'react';
 import { useList } from '@keystone-6/core/admin-ui/context';
 import { CreateItemDrawer } from '@keystone-6/core/admin-ui/components';
 import { ItemDrawer } from './components/ItemDrawer';
+import { gql, useQuery } from '@keystone-6/core/admin-ui/apollo';
 
 export const Field = (props: FieldProps<typeof controller>) => {
   // Get metadata properties using a custom hook
@@ -57,6 +58,31 @@ function ComponentWrapper(props: {
   const [isCreateDrawerOpen, setIsCreateDrawerOpen] = useState(false)
   const [isEditDrawerOpen, setIsEditDrawerOpen] = useState<string | undefined>()
 
+  // After onCreate, this gets the object needed for display in "OrderableList"
+  const newForeignItemQuery = useQuery(
+    gql`query($where: ${foreignList.gqlNames.whereUniqueInputName}!) {
+      ${foreignList.gqlNames.itemQueryName}(where: $where) {
+        ${props.foreignLabelPath}
+        id
+      }
+    }`,
+    {
+      // "variables" must be present but empty
+      // This value is via args of ".refetch()"
+      variables: {}
+    }
+  )
+  const onCreate = async (item: Item) => {
+    const res = await newForeignItemQuery.refetch({
+      where: {
+        id: item.value
+      }
+    })
+    props.onChange([...props.value, res.data[foreignList.gqlNames.itemQueryName]])
+  }
+
+  // Item[] is used for display purposes
+  // This converts it to the correct type for the Field's onChange
   const onChange = (items: Item[]) => {
     props.onChange(items.map(i => ({ [props.foreignLabelPath]: labels.find((option: any) => option.label === i.label).value, id: i.value })))
   }
@@ -94,7 +120,7 @@ function ComponentWrapper(props: {
             }}
             onCreate={val => {
               setIsCreateDrawerOpen(false)
-              onChange([...value, { label: val.label, value: val.id }])
+              onCreate({ label: val.label, value: val.id })
             }}
           />
         </DrawerController>
